5b85dddfa80c6ef6f92178a04124e038594c31c5
Felipe Kauãn <felipekauan30@outlook.com>
2026-02-16 16:14:13 -0300

    feat: adiciona inicialização de objetos e exibição da estante no index

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 require_once 'vendor/autoload.php';
+
+use Felipe\Biblioteca\Livro;
+use Felipe\Biblioteca\Estante;
+use Felipe\Biblioteca\Aluno;
+
+// objeto estante
+$estante = new Estante();
+
+// objetos alunos
+$aluno1 = new Aluno('Paulo');
+$aluno2 = new Aluno('Roberto');
+
+// objetos livros
+$livro1 = new Livro('Chico Bento', 'Monteiro Lobato', true);
+$livro2 = new Livro('Python', 'Guanabara', true);
+
+// adicionando livros na estante
+$estante->adicionarLivro($livro1);
+$estante->adicionarLivro($livro2);
+
+// mostra a estante
+echo "<pre>";
+print_r($estante);
+echo "<hr>";
 ?>
